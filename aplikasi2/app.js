19f7c063def18ce9271d941e52375f590e64b105
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-09-19 15:41:35 +0700

    Update app.js
    update ke dua

--- aplikasi2/app.js
+++ aplikasi2/app.js
@@ -1,26 +1,26 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 // Route Get
-app.get('/', (req, res) => {
-    res.send('Hello, ini halaman HOME dengan method GET');
+app.get("/", (req, res) => {
+  res.send("Hello, ini halaman HOME dengan method GET");
 });
 
-// middleware untuk parsinf body request 
+// middleware untuk parsinf body request
 app.use(express.json());
 
 // Route POST
-app.post('/submit', (req, res) => {
-    const {name, npm, jeniskelamin} = req.body;
-    res.send('hallo, ${name} dengan NPM ${npm}. Apakah kamu ${jeniskelamin}?');
+app.post("/submit", (req, res) => {
+  const { name, npm, jeniskelamin } = req.body;
+  res.send("hallo, ${name} dengan NPM ${npm}. Apakah kamu ${jeniskelamin}?");
 });
 
 //Serving static file
 // menulis css dengan menggunakan express
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(port, (req, res) => {
-    console.log('Server Running at http://localhost:${port}');
+  console.log("Server Running at http://localhost:${port}");
 });
