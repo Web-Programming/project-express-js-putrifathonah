673e5b88db1e6ed399bba4321c1c94fd100eb33b
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-09-29 15:30:41 +0700

    fitur search

--- toko-online/routes/index.js
+++ toko-online/routes/index.js
@@ -7,6 +7,23 @@ router.get("/", function (req, res, next) {
   res.render("index", {
     title: "Toko Online Sederhana",
     products: products,
+    search: "", // supaya tidak undefined di EJS
+  });
+});
+
+/* GET search page */
+router.get("/search", function (req, res, next) {
+  const q = req.query.q ? req.query.q.toLowerCase() : "";
+  let filteredProducts = products;
+
+  if (q) {
+    filteredProducts = products.filter((p) => p.name.toLowerCase().includes(q));
+  }
+
+  res.render("index", {
+    title: "Toko Online Sederhana",
+    products: filteredProducts,
+    search: q,
   });
 });
 
