2901ddc133c6a2a3d491247d8f4f866964c56c76
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-10-13 17:44:33 +0700

    CRUD API

--- toko-online/app.js
+++ toko-online/app.js
@@ -3,18 +3,19 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-// panggil fule koleksi database
+//panggil file koneksi database
 require("./app_toko_online/models/db");
 
-// perbaikan ke dua
-var indexRouter = require("../toko-online/app_toko_online/routes/index");
-var usersRouter = require("../toko-online/app_toko_online/routes/users");
-var productRouter = require("../toko-online/app_toko_online/routes/product"); // letakkan router produk
+//Perbaikan ke 2
+var indexRouter = require("./app_toko_online/routes/index");
+var usersRouter = require("./app_toko_online/routes/users");
+var productRouter = require("./app_toko_online/routes/product"); //letakkan di atas agar rapi
+var apiProductRouter = require("./app_toko_online/routes/api/product"); //import route api
 var engine = require("ejs-blocks"); //menggunakan ejs block
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "app_toko_online", "views")); // perbaikan 1
+app.set("views", path.join(__dirname, "app_toko_online", "views")); //perbaikan 1
 app.engine("ejs", engine); //daftarkan engine ejs block
 app.set("view engine", "ejs");
 
@@ -23,17 +24,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// routers
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/produk", productRouter);
-
 //serving bootstrap
 app.use(
   "/bootstrap",
   express.static(path.join(__dirname, "node_modules/bootstrap/dist"))
 );
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/produk", productRouter);
+app.use("/api/produk", apiProductRouter); //daftarkan route api
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
