8529691e695aa2f29c2b5869e2e953777afad8e5
putrifathonah <putrifathonah_2327240139@mhs.mdp.ac.id>
2025-10-24 11:08:22 +0700

    Middleware

--- toko-online/app_toko_online/routes/middleware/authMiddleware.js
+++ toko-online/app_toko_online/routes/middleware/authMiddleware.js
@@ -1,14 +1,13 @@
 exports.adminOnly = (req, res, next) => {
-    const isAdmin = req.body.isAdmin; // contoh 'isAdmin'
-    true
-    if (isAdmin === true){
-        console.log("middleware: Akses admin diberikan");
-        next(); // lanjutkan
-    }else {
-        // 403 forthhiddem
-        return res.status(403).json({
-            success: false,
-            message: "Akses Ditolak. Endpoint ini membutuhkan hak admin."
-        });
-    }
+  const isAdmin = req.body.isAdmin; // Contoh: { "isAdmin": true }
+  if (isAdmin === true) {
+    console.log("Middleware: Akses Admin Diberikan.");
+    next(); // Lanjutkan
+  } else {
+    // 403 Forbidden
+    return res.status(403).json({
+      success: false,
+      message: "Akses Ditolak. Endpoint ini membutuhkan hak Admin.",
+    });
+  }
 };
